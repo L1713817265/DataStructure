4e683b7063615e6f3335e11f125811601874b64c
L1713817265 <1713817265@qq.com>
2023-12-18 11:12:14 +0800

    [新增] 动态数组的头文件(.h)和源文件(.c)

---
+++ dynamicArray/dynamicArray.h
@@ -0,0 +1,7 @@
+#ifndef __DYNAMIC_ARRAY_H_
+#define __DYNAMIC_ARRAY_H_
+
+//   避免头文件重复
+
+
+#endif //__DYNAMICARRAY_H_
---
+++ dynamicArray/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dynamicArray.h"
+
+int marmin()
+{
+    //  静态数组
+    //      1. 不够灵活
+    //      2. 可能会浪费空间
+
+
+
+    return 0;
+}
