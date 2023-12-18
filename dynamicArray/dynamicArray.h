60ff480a32e2517649c88d4758b6ebbffa00de3e
L1713817265 <1713817265@qq.com>
2023-12-18 11:18:52 +0800

    [修改] 动态数组的节点

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,7 +1,16 @@
 #ifndef __DYNAMIC_ARRAY_H_
 #define __DYNAMIC_ARRAY_H_
 
+
+typedef int ELEMENTTYPE;
 //   避免头文件重复包含
+
+typedef dynamicArray
+{
+    ELEMENTTYPE *data;      //数组的空间
+    int len;        //数组的大小
+    int capacity;   //数组的容量
+}dynamicArray;
 
 
 #endif //__DYNAMICARRAY_H_
