0fb51311d9820f8b45a3f575701d093330b6791a
L1713817265 <1713817265@qq.com>
2023-12-18 11:41:44 +0800

    [新增] 新增动态数组的API接口

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -12,5 +12,34 @@ typedef dynamicArray
     int capacity;   //数组的容量
 }dynamicArray;
 
+//  API: application program interface
+
+//  动态数组的初始化
+int dynamicArrayInit();
+
+//  动态数组插入数据(默认插到数组的末尾)
+int dynamicArrayInsertData();
+
+//  动态数组插入数据, 在指定位置插入
+int dynamicArrayAppoinPosInsertData();
+
+//  动态数组修改指定位置的数据
+int dynamicArrayModifyAppointPosData();
+
+//  动态数组删除数据(默认情况下删除最后末尾的数据)
+int dynamicArrayDelelteData();
+
+//  动态数组删除指定位置的数据
+int dynamicArrayDeleteAppointPosData();
+
+//  动态数组的销毁
+int dynamicArrayDestroy();
+
+//  获取数组的大小
+int dynamicArrayGetSize();
+
+//  获取数组的容量
+int dynamicArrayGetCapacity();
+
 
 #endif //__DYNAMICARRAY_H_
