b9a98db4d38ef9bf8777442e5ff6a90be7c3d674
L1713817265 <1713817265@qq.com>
2023-12-20 11:26:33 +0800

    [修改] 完善链表的遍历操作

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -156,5 +156,28 @@ int LinkListDestroy(LinkList *pList)
 //  链表遍历接口
 int LinkListForeach(LinkList *pList)
 {
+    int ret = 0;
 
+    if(!pList)
+    {
+        return NULL_PTR;
+    }
+
+#if 0
+    LinkNode *travelNode = pList->head;
+    while(travelNode->next != NULL)
+    {
+        travelNode = travelNode->next;
+        printf("travelNode->data: %d",travelNode->data);
+    }
+#else
+    LinkNode *travelNode = pList->head->next;
+    while(travelNode != NULL)
+    {
+        printf("travelNode->data: %d",travelNode->data);
+        travelNode = travelNode->next;
+    }
+#endif
+
+    return ret;
 }
